2e99411fe83c36db543f69ccd24cd81bcfc802cf
Roc Meseguer <roc.meseguer@upc.edu>
2025-05-12 13:26:18 +0200

    Canvis 2024-25-Q2

--- server/src/sockets/socket.ts
+++ server/src/sockets/socket.ts
@@ -1,32 +1,30 @@
-
 import SocketIO from "socket.io";
 import * as http from "http";
 import { IMessage } from "../models/message";
 
-const initializeSocket = (server: http.Server) => {
-
-    const io = new SocketIO.Server(server,  {
+const createSocketServer = (server: http.Server) => {
+    return new SocketIO.Server(server, {
         cors: {
-          origin: "*"
+            origin: "*"
         }
     });
+};
 
-
+const initializeSocket = (server: http.Server) => {
+    const io = createSocketServer(server);
     io.on('connection', (socket: SocketIO.Socket) => {
       console.log('Connected client', socket.id);
 
       socket.on("message", (m: IMessage) => {
-          console.log("[server](message): %s", JSON.stringify(m));
-          io.emit("message", m);
-        });
-
-      socket.on('disconnect', () => {
-        console.log("Client disconnected", socket.id);
-        socket.disconnect();
+          console.log("[server](message): %s", m.content);
+          socket.emit("message", m);
       });
-            
+  
+      socket.on('disconnect', () => {
+          console.log("Client disconnected", socket.id);
+          socket.disconnect();
+      });
     });
-        
 };
 
 export default initializeSocket;
